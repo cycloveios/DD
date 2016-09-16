e52d97ca793aaf3f46277b7b89fdf352de34e2e8
蔡叶超 <caiyechao@123feng.com>
2016-09-16 13:20:16 +0800

    chore： add podfile

--- DD/ViewController.swift
+++ DD/ViewController.swift
@@ -7,19 +7,40 @@
 //
 
 import UIKit
+import SnapKit
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+      
+      let btn = UIButton()
+      btn.setTitle("23e", forState: .Normal)
+      view.addSubview(btn)
+    
+     let label = UILabel()
+      view.addSubview(label)
+      label.snp_makeConstraints { (make) in
+        make.left.equalTo(1)
+        make.top.equalTo(0.6)
+      }
+      
+
+      
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
+        
     }
 
+    func did() {
+        
+    }
 
 }
 
